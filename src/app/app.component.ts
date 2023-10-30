005bf6ef43150198abc0c2f53a273f4ec3712ac0
mathankumar.s <mathankumar.s@200411LTP2851>
2023-10-30 10:46:28 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveforms';
+  userData:any=[];
+  isShow:boolean=false;
+  onSubmit(){
+    if(this.userForm.valid){
+    console.log(this.userForm.value);
+    this.userData.push(this.userForm.value);
+    this.isShow = true;
+    }
+  else{
+    console.log("invalid");
+    this.isShow = false;
+  }
+  }
+  userForm= new FormGroup({
+    firstName: new FormControl('',[Validators.minLength(5) ,Validators.maxLength(9),Validators.required]),
+    lastName: new FormControl('',[Validators.minLength(5) ,Validators.maxLength(9),Validators.required]),
+    joinDate: new FormControl('03-01-2020',[Validators.required]),
+    salary: new FormControl(0,[Validators.required])
+  },{
+    updateOn:'change'
+  }
+  );
+ 
 }
